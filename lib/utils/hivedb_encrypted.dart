eb09235ad6626ebe9392588b7fda5d518b5637cd
f3公共编译账户 <11203408+f3-common@user.noreply.gitee.com>
2023-02-11 19:20:56 +0800

    feat: store db key local

--- lib/utils/hivedb_encrypted.dart
+++ lib/utils/hivedb_encrypted.dart
@@ -1,15 +1,34 @@
 import 'dart:convert';
 
+import 'package:f3_wallet/model/storage_item.dart';
+import 'package:f3_wallet/utils/secure_storeage.dart';
 import 'package:hive/hive.dart';
 
 const dbName = "keyBoxEncrypted";
 List<int> dbKey = [];
 
-Future<void> dbInsert(dynamic key, dynamic value) async {
+Future<void> initKey() async {
   if (dbKey.isEmpty) {
-    dbKey = Hive.generateSecureKey();
-    print(base64UrlEncode(key));
+    final StorageService _storageService = StorageService();
+
+    final storedKey =
+        await _storageService.readSecureData("f3wallet_hivedb_key");
+
+    if (storedKey != null) {
+      dbKey = base64Url.decode(storedKey);
+    } else {
+      dbKey = Hive.generateSecureKey();
+      final encodedKey = base64UrlEncode(dbKey);
+      print(encodedKey);
+      final StorageItem newItem =
+          StorageItem("f3wallet_hivedb_key", encodedKey);
+      _storageService.writeSecureData(newItem);
+    }
   }
+}
+
+Future<void> dbInsert(dynamic key, dynamic value) async {
+  await initKey();
   final encryptedBox =
       await Hive.openBox(dbName, encryptionCipher: HiveAesCipher(dbKey));
   encryptedBox.put(key, value);
@@ -17,10 +36,7 @@ Future<void> dbInsert(dynamic key, dynamic value) async {
 }
 
 Future<dynamic> dbGet(dynamic key) async {
-  if (dbKey.isEmpty) {
-    dbKey = Hive.generateSecureKey();
-    print(base64UrlEncode(key));
-  }
+  await initKey();
   final encryptedBox =
       await Hive.openBox(dbName, encryptionCipher: HiveAesCipher(dbKey));
 
