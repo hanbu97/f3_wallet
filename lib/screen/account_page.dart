dd63d2f1b25aae96e75c7b5eb8e241eb9dfb3ac1
f3公共编译账户 <11203408+f3-common@user.noreply.gitee.com>
2023-02-11 17:58:19 +0800

    chore: folder multisig/single

--- lib/screen/account_page.dart
+++ lib/screen/account_page.dart
@@ -2,20 +2,18 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:animate_do/animate_do.dart';
-import 'package:f3_wallet/screen/multsig_approve_page.dart';
-import 'package:f3_wallet/screen/multsig_cancel_page.dart';
-import 'package:f3_wallet/screen/multsig_create_page%20copy.dart';
+import 'package:f3_wallet/screen/multisig/multsig_approve_page.dart';
+import 'package:f3_wallet/screen/multisig/multsig_cancel_page.dart';
+import 'package:f3_wallet/screen/multisig/multsig_create_page%20copy.dart';
 import 'package:f3_wallet/screen/home_view.dart';
-import 'package:f3_wallet/screen/multsig_send_page%20copy.dart';
+import 'package:f3_wallet/screen/multisig/multsig_send_page%20copy.dart';
 import 'package:f3_wallet/screen/result_page.dart';
-import 'package:f3_wallet/screen/single_send_page.dart';
+import 'package:f3_wallet/screen/single/single_send_page.dart';
 import 'package:f3_wallet/services/lotus.dart';
 import 'package:f3_wallet/shared/app_colors.dart';
-import 'package:f3_wallet/widget/asset_card.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/services.dart';
-import 'package:auto_animated/auto_animated.dart';
 
 class AccountPage extends StatefulWidget {
   final String address;
